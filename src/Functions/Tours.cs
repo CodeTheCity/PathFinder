bb74ecb4f3fc97798aa4e4cc953cf3c59cbd4226
ben.owen <b.l.owen@outlook.com>
2018-02-04 14:55:26 +0000

    Allow clients to retrieve info on a single Tour entity.

--- src/Functions/Tours.cs
+++ src/Functions/Tours.cs
@@ -31,5 +31,32 @@ namespace Functions {
                 };
             }
         }
+
+        [FunctionName("GetTour")]
+        public static HttpResponseMessage GetTour(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "Tours/{tourId}")]HttpRequestMessage req,
+            int tourId,
+            TraceWriter log) {
+
+            using (DataContext context = new DataContext()) {
+                var tour = context.Tours
+                    .Where(row => row.TourId == tourId)
+                    .Select(row => new {
+                        row.TourId,
+                        row.Name,
+                    })
+                    .SingleOrDefault();
+
+                if (tour == null) {
+                    return new HttpResponseMessage(HttpStatusCode.NotFound);
+                }
+
+                string json = JsonConvert.SerializeObject(tour);
+
+                return new HttpResponseMessage(HttpStatusCode.OK) {
+                    Content = new StringContent(json, Encoding.UTF8, "application/json")
+                };
+            }
+        }
     }
 }
