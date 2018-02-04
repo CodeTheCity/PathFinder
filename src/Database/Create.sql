0c63034db1d08f9cc06215caeeade8264fd5f898
ben.owen <b.l.owen@outlook.com>
2018-02-04 13:02:16 +0000

    Fix optional columns that had been declared as NOT NULL.

--- src/Database/Create.sql
+++ src/Database/Create.sql
@@ -8,14 +8,14 @@ CREATE TABLE Waypoint (
     [WaypointId] int NOT NULL PRIMARY KEY,
     [TourId] int NOT NULL FOREIGN KEY REFERENCES Tour,
     [Name] nvarchar(50) NOT NULL,
-    Latitude decimal NOT NULL,
-    Longitude decimal NOT NULL,
+    Latitude decimal(18,9) NOT NULL,
+    Longitude decimal(18,9) NOT NULL,
     Description nvarchar(200) NOT NULL,
     HasFee bit NOT NULL,
     IsAccessible bit NOT NULL,
     HasParking bit NOT NULL,
-    Url nvarchar(200) NOT NULL,
-    TelephoneNumber nvarchar(20) NOT NULL,
+    Url nvarchar(200) NULL,
+    TelephoneNumber nvarchar(20) NULL,
     Category nvarchar(20) NOT NULL
 )
 GO
